4ee3592c42bfba71c37c249f09affdc6afc98e58
Lawrence Wong <oceandeep@gmail.com>
2022-10-24 23:06:35 +0800

    20221024

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,10 +4,21 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
+    
+    protected User() {
+	
+    }
+    
+    @Id
+    @GeneratedValue
     private Integer id;
     
     @Size(min=2, message = "Name should have at least 2 characters")
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(id, birth_date, name)
+values(10001, current_date, 'Lawrence Wong');
+
+insert into user_details(id, birth_date, name)
+values(10002, current_date, 'Jennie Ho');
+
+insert into user_details(id, birth_date, name)
+values(10003, current_date, 'Kayden Wong');
+
+insert into user_details(id, birth_date, name)
+values(10004, current_date, 'Kaylee Wong');

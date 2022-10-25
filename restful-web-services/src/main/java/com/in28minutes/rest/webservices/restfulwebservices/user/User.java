ddd17987c160eb19c13061f2d7720aad9717bda6
Lawrence Wong <oceandeep@gmail.com>
2022-10-25 22:38:08 +0800

    20221025

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,33 +1,40 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 @Entity(name = "user_details")
 public class User {
-    
+
     protected User() {
-	
+
     }
-    
+
     @Id
     @GeneratedValue
     private Integer id;
-    
-    @Size(min=2, message = "Name should have at least 2 characters")
+
+    @Size(min = 2, message = "Name should have at least 2 characters")
     @JsonProperty("user_name")
     private String name;
-    
+
     @Past(message = "Birth Date should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
 	super();
@@ -62,7 +69,8 @@ public class User {
 
     @Override
     public String toString() {
-	return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate
+		+ "]";
     }
 
 }
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -9,3 +9,15 @@ values(10003, current_date, 'Kayden Wong');
 
 insert into user_details(id, birth_date, name)
 values(10004, current_date, 'Kaylee Wong');
+
+insert into post(id, description, user_id)
+values(20001, 'I want to learn AWS', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I want to learn DevOps', 10001);
+
+insert into post(id, description, user_id)
+values(20003, 'I want get AWS certificated', 10002);
+
+insert into post(id, description, user_id)
+values(20004, 'I want to learn multi cloud', 10002);

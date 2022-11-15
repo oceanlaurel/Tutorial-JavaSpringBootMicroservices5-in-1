c9147772b916eaed3500f7bea56a8a858e91bec9
Lawrence Wong <oceandeep@gmail.com>
2022-11-15 22:29:06 +0800

    20221115

--- naming-server/src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
@@ -2,12 +2,14 @@ package com.in28minutes.microservices.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NamingServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NamingServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(NamingServerApplication.class, args);
+    }
 
 }

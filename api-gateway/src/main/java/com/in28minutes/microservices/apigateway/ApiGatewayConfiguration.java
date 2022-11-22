918323170b1d79ceb5669c9aa86c475387638f9d
Lawrence Wong <oceandeep@gmail.com>
2022-11-22 22:00:05 +0800

    20221122

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -15,13 +15,31 @@ public class ApiGatewayConfiguration {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-	Function<PredicateSpec, Buildable<Route>> routeFunction = p -> p
-		.path("/get")
-		.filters(f -> f.addRequestHeader("MyHeader", "MyURI")
-		.addRequestParameter("Param", "MyValue"))
-		.uri("http://httpbin.org:80");
+//	Function<PredicateSpec, Buildable<Route>> routeFunction = p -> p
+//		.path("/get")
+//		.filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+//		.addRequestParameter("Param", "MyValue"))
+//		.uri("http://httpbin.org:80");
+//
+//	return builder.routes().route(routeFunction).build();
 
-	return builder.routes().route(routeFunction).build();
+	return builder.routes()
+		.route(p -> p.path("/get")
+			.filters(f -> f.addRequestHeader("MyHeader", "MyURL")
+				.addRequestParameter("Param", "MyValue"))
+			.uri("http://httpbin.org:80"))
+		.route(p -> p.path("/currency-exchange/**")
+			.uri("lb://currency-exchange"))
+		.route(p -> p.path("/currency-conversion/**")
+			.uri("lb://currency-conversion"))
+		.route(p -> p.path("/currency-conversion-feign/**")
+			.uri("lb://currency-conversion"))
+		.route(p -> p.path("/currency-conversion-new/**")
+			.filters(f -> f.rewritePath(
+				"/currency-conversion-new/(?<segment>.*)",
+				"/currency-conversion-feign/${segment}"))
+			.uri("lb://currency-conversion"))
+		.build();
     }
 
 }

a4a591b7176a1491d5af7d0c09e4e91d39f1c776
Lawrence Wong <oceandeep@gmail.com>
2022-11-23 22:46:58 +0800

    20221123

---
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,32 @@
+package com.in28minutes.microservices.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+	    GatewayFilterChain chain) {
+	logger.info("Path of the request received -> {}",
+		exchange.getRequest().getPath());
+
+	logger.info("From remote: {} -> To local: {}",
+		(exchange.getRequest().getRemoteAddress().getHostName() + ":"
+			+ exchange.getRequest().getRemoteAddress().getPort()),
+		(exchange.getRequest().getLocalAddress().getHostName() + ":"
+			+ exchange.getRequest().getLocalAddress().getPort()));
+
+	return chain.filter(exchange);
+    }
+
+}
